6d6d543edc8261e372e931117b9869b54af6c1a2
Malek Ben Dali <bendali.malekk@gmail.com>
2021-04-12 16:37:24 +0100

    added tabs

--- src/apps/MealApp/navigation/MealsNavigator.js
+++ src/apps/MealApp/navigation/MealsNavigator.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {Platform} from 'react-native';
 import {
-  CategoryDetailScreen,
   CategoryMealsScreen,
   CategoriesScreen,
   FavoritesScreen,
@@ -10,8 +9,11 @@ import {
   MealDetailScreen,
 } from '../screens';
 import {colors} from '../assets';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const option = {
   headerStyle: {
@@ -23,14 +25,46 @@ const option = {
 const MealsNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Category" screenOptions={option}>
-      <Stack.Screen name="CategoryDetail" component={CategoryDetailScreen} />
       <Stack.Screen name="CategoryMeals" component={CategoryMealsScreen} />
       <Stack.Screen name="Category" component={CategoriesScreen} />
-      <Stack.Screen name="Favorites" component={FavoritesScreen} />
-      <Stack.Screen name="Filter" component={FilterScreen} />
       <Stack.Screen name="MealDetail" component={MealDetailScreen} />
     </Stack.Navigator>
   );
 };
+const Favorites = () => {
+  return (
+    <Stack.Navigator initialRouteName="Favorites" screenOptions={option}>
+      <Stack.Screen name="Favorites" component={FavoritesScreen} />
+      <Stack.Screen name="Filter" component={FilterScreen} />
+    </Stack.Navigator>
+  );
+};
 
-export default MealsNavigator;
+const tabBarOptions = {
+  activeTintColor: colors.secondary,
+};
+
+const Tabs = () => (
+  <Tab.Navigator tabBarOptions={tabBarOptions}>
+    <Tab.Screen
+      name="Meals"
+      component={MealsNavigator}
+      options={{
+        tabBarIcon: tabInfo => (
+          <Ionicons name="restaurant-outline" size={25} color={tabInfo.color} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Favorites"
+      component={Favorites}
+      options={{
+        tabBarIcon: tabInfo => {
+          return <Ionicons name="ios-star" size={25} color={tabInfo.color} />;
+        },
+      }}
+    />
+  </Tab.Navigator>
+);
+
+export default Tabs;
--- src/apps/MealApp/screens/index.js
+++ src/apps/MealApp/screens/index.js
@@ -1,12 +1,10 @@
 import {CategoriesScreen} from './CategoriesScreen';
-import CategoryDetailScreen from './CategoryDetailScreen';
 import CategoryMealsScreen from './CategoryMealsScreen';
 import FavoritesScreen from './FavoritesScreen';
 import FilterScreen from './FilterScreen';
 import MealDetailScreen from './MealDetailScreen';
 
 export {
-  CategoryDetailScreen,
   CategoryMealsScreen,
   CategoriesScreen,
   FavoritesScreen,
